f8c178065315001f14b341be6e6856e831ae56f1
RyZum <ryzumm@gmail.com>
2020-02-02 21:59:01 +0100

    Basic bot with basic commands
    - Deployable to Azure Webservices within VS
    - Fetch token from Azure keyvault
    - Uses Discord.Net and Command library

---
+++ Modules/TestModule.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace BJR_bot.Modules
+{
+    // Modules must be public and inherit from an IModuleBase
+    public class TestModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        public Task PingAsync()
+            => ReplyAsync("pong!");
+
+       // Get info on a user, or the user who invoked the command if one is not specified
+        [Command("userinfo")]
+        public async Task UserInfoAsync(IUser user = null)
+        {
+            user ??= Context.User;
+
+            await ReplyAsync(user.ToString());
+        }
+
+       // [Remainder] takes the rest of the command's arguments as one argument, rather than splitting every space
+        [Command("echo")]
+        public Task EchoAsync([Remainder] string text)
+            // Insert a ZWSP before the text to prevent triggering other bots!
+            => ReplyAsync('\u200B' + text);
+
+        // 'params' will parse space-separated elements into a list
+        [Command("list")]
+        public Task ListAsync(params string[] objects)
+            => ReplyAsync("You listed: " + string.Join("; ", objects));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+using BJR_bot.Services;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BJR_bot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        // Discord.Net heavily utilizes TAP for async, so we create
+        // an asynchronous context from the beginning.
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            var token = await GetTokenAsync();
+
+            using (var services = ConfigureServices())
+            {
+                _client = services.GetRequiredService<DiscordSocketClient>();
+
+                _client.Log += LogAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
+
+                _client.Ready += ReadyAsync;
+
+                await _client.LoginAsync(TokenType.Bot, token);
+                await _client.StartAsync();
+
+                // Here we initialize the logic required to register our commands.
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                // Block the program until it is closed.
+                await Task.Delay(-1);
+            }
+        }
+
+        private async Task<string> GetTokenAsync()
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            KeyVaultClient keyVaultClient =
+                new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            var secret = await keyVaultClient
+                .GetSecretAsync(
+                    "https://discord-bot-keyvault.vault.azure.net/secrets/bot-token/f1b36f4771cf4671b9c0e43d963c625c")
+                .ConfigureAwait(false);
+            string token = secret.Value;
+            return token;
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        // The Ready event indicates that the client has opened a
+        // connection and it is now safe to access the cache.
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .BuildServiceProvider();
+        }
+    }
+}
